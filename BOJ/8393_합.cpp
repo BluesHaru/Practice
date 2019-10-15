02d072b8de0a96b3e7fbc7cb34442d3614c00462
Spes우폴 <guinn0098@gmail.com>
2019-10-15 12:43:07 +0900

    Update

--- BOJ/8393_합.cpp
+++ BOJ/8393_합.cpp
@@ -1,5 +1,7 @@
 // 8393 합
 // 입출력
+// 문제 : 1부터 n까지의 합
+// 입력 조건 : (1<= n <= 10,000)
 
 #include <iostream>
 
@@ -11,12 +13,15 @@ int main()
 	cin.tie(0);
 
 	int N, result = 0;
+	
+	// 숫자 입력
 	cin >> N;
 	do
 	{
 		result += N;
 	} while (--N > 0);
 
+	// 결과 출력
 	cout << result << '\n';
 
 	return 0;
