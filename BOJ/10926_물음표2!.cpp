4b8a1075439946dffaf3ed7f9ff73a22849035b0
Woocheol Yang <guinn0098@gmail.com>
2019-12-22 16:31:59 +0900

    2019/12/22

---
+++ BOJ/10926_물음표2!.cpp
@@ -0,0 +1,19 @@
+/*---------------------------------------------------
+	10926 ??!
+	출력
+	문제정보 : ??!을 붙여 출력하라. (trigraph)
+	조건 :
+		Input)
+		Output)
+----------------------------------------------------*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string text;
+    cin >> text;
+    cout << text << "??!" << '\n';
+    return 0;
+}
